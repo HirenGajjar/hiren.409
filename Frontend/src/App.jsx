d7b557c0d36f2e7cf8062305e5228cf2dfabd2de
HirenGajjar <gajjarhiren111@gmail.com>
2024-10-15 23:50:09 -0700

    Add full screen pre loader component

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,30 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import MainComponent from "./Components/MainComponent/MainComponent";
 import NavBar from "./Components/NavBar/NavBar";
+import FullScreenLoader from "./Components/FullScreenLoader.jsx/FullScreenLoader";
+
 const App = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <>
-      <div className=" w-full h-screen">
-        <NavBar />
-        <MainComponent />
+      <div className="w-full h-screen">
+        {loading ? (
+          <FullScreenLoader />
+        ) : (
+          <>
+            <NavBar />
+            <MainComponent />
+          </>
+        )}
       </div>
     </>
   );
