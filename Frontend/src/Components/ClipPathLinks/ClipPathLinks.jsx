e5df5d2184a179c16c2d5e4f7c5e6c4c4d9515f7
HirenGajjar <gajjarhiren111@gmail.com>
2024-10-16 20:10:32 -0700

    Fix icons on skill component

--- Frontend/src/Components/ClipPathLinks/ClipPathLinks.jsx
+++ Frontend/src/Components/ClipPathLinks/ClipPathLinks.jsx
@@ -2,34 +2,51 @@
 import React from "react";
 import LinkBox from "../../AnimationComponents/LinkBox/LinkBox";
 import {
-  SiAdobe,
-  SiApple,
-  SiFacebook,
-  SiGoogle,
-  SiLinkedin,
-  SiShopify,
-  SiSoundcloud,
-  SiSpotify,
-  SiTiktok,
+  SiNodedotjs,
+  SiTypescript,
+  SiGit,
+  SiDocker,
+  SiReact,
+  SiNextdotjs,
+  SiTailwindcss,
+  SiBootstrap,
+  SiSass,
+  SiMongodb,
+  SiPostgresql,
+  SiJsonwebtokens,
 } from "react-icons/si";
+import { FaAws } from "react-icons/fa";
+
+import { GrMysql } from "react-icons/gr"; // MySQL uses Gr icon set
 
 const ClipPathLinks = () => {
   return (
     <div className="divide-y divide-neutral-900 border border-neutral-900">
       <div className="grid grid-cols-2 divide-x divide-neutral-900">
-        <LinkBox Icon={SiGoogle} href="#" />
-        <LinkBox Icon={SiShopify} href="#" />
-      </div>
-      <div className="grid grid-cols-4 divide-x divide-neutral-900">
-        <LinkBox Icon={SiApple} href="#" />
-        <LinkBox Icon={SiSoundcloud} href="#" />
-        <LinkBox Icon={SiAdobe} href="#" />
-        <LinkBox Icon={SiFacebook} href="#" />
+        <LinkBox Icon={SiNodedotjs} href="#" />
+        <LinkBox Icon={SiTypescript} href="#" />
       </div>
       <div className="grid grid-cols-3 divide-x divide-neutral-900">
-        <LinkBox Icon={SiTiktok} href="#" />
-        <LinkBox Icon={SiSpotify} href="#" />
-        <LinkBox Icon={SiLinkedin} href="#" />
+        <LinkBox Icon={SiGit} href="#" />
+        <LinkBox Icon={SiDocker} href="#" />
+        <LinkBox Icon={FaAws} href="#" />
+      </div>
+      <div className="grid grid-cols-2 divide-x divide-neutral-900">
+        <LinkBox Icon={SiReact} href="#" />
+        <LinkBox Icon={SiNextdotjs} href="#" />
+      </div>
+      <div className="grid grid-cols-3 divide-x divide-neutral-900">
+        <LinkBox Icon={SiTailwindcss} href="#" />
+        <LinkBox Icon={SiBootstrap} href="#" />
+        <LinkBox Icon={SiSass} href="#" />
+      </div>
+      <div className="grid grid-cols-4 divide-x divide-neutral-900">
+        <LinkBox Icon={GrMysql} href="#" />
+        <LinkBox Icon={SiPostgresql} href="#" />
+        <LinkBox Icon={SiMongodb} href="#" />
+        <LinkBox Icon={SiJsonwebtokens} href="#" />
+
+        {/* Reused AWS for database support */}
       </div>
     </div>
   );
