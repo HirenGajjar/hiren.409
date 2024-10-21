d6ce7e15d7a34fa41c25fcd2b9069e7d7c8bd1d5
HirenGajjar <gajjarhiren111@gmail.com>
2024-10-21 01:25:29 -0700

    Add heading for skills section

--- Frontend/src/Components/Headings/Headings.jsx
+++ Frontend/src/Components/Headings/Headings.jsx
@@ -2,11 +2,28 @@ import React, { useState, useEffect } from "react";
 import Marquee from "react-fast-marquee";
 
 function Headings({ heading }) {
+  const [speed, setSpeed] = useState(350);
   const repeatedWord = `\u00A0\u00A0${heading}\u00A0\u00A0`.repeat(1000).trim();
+  const updateSpeed = () => {
+    if (window.innerWidth < 640) {
+      setSpeed(150);
+    } else {
+      setSpeed(350);
+    }
+  };
+  useEffect(() => {
+    updateSpeed();
+
+    window.addEventListener("resize", updateSpeed);
+
+    return () => {
+      window.removeEventListener("resize", updateSpeed);
+    };
+  }, []);
   return (
     <>
-      <Marquee speed="350" gradient="true">
-        <h2 className="uppercase sm:text-4xl md:text-6xl lg:text-8xl  py-4">
+      <Marquee speed={speed}>
+        <h2 className="uppercase text-3xl md:text-6xl lg:text-8xl  py-4">
           {repeatedWord}
         </h2>
       </Marquee>
--- Frontend/src/Components/SkillsSection/SkillsSection.jsx
+++ Frontend/src/Components/SkillsSection/SkillsSection.jsx
@@ -6,9 +6,8 @@ import Headings from "../Headings/Headings";
 const SkillSection = () => {
   return (
     <>
-      <Headings heading="Skills" />
-
       <div className="bg-neutral-50 px-4 py-12 ">
+        <Headings heading="Skills" />
         <div className="mx-auto max-w-7xl my-12">
           <ClipPathLinks />
         </div>
