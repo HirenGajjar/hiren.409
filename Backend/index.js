22a14c4aa4e42b580ede6fd7a27560c016351cb7
HirenGajjar <gajjarhiren111@gmail.com>
2024-11-05 17:32:52 -0800

    Add bodyparser and cors in server

--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,17 @@
-//Start the server
 const express = require("express");
 const app = express();
-
+const dotenv = require("dotenv");
 const cors = require("cors");
-
+const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 5000;
 
+const connectDB = require("./db");
+// Configs
+dotenv.config();
+// Connect to Database
+connectDB();
+//Middlewares
+app.use(cors({ origin: "http://localhost:5173" }));
+app.use(bodyParser.json());
 // Start the server
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
