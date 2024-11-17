5b340e21972c2760fe7e71c3e6b2bafb7f247d80
HirenGajjar <gajjarhiren111@gmail.com>
2024-11-16 19:01:58 -0800

    Fix database connection

--- Backend/db.js
+++ Backend/db.js
@@ -2,10 +2,16 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
+    // await mongoose.connect(process.env.MONGODB_URI, {
+    //   useNewUrlParser: true,
+    //   useUnifiedTopology: true,
+    // });
+    await mongoose.connect("mongodb://localhost:27017/hiren_409_messages", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
+
+    //mongodb://localhost:27017/hiren_409_messages
     console.log("Database connected!");
   } catch (error) {
     console.error("Failed to connect! : ", error.message);
